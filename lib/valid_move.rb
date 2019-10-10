f834f528c34de990c84ddc6c8c95fb74c63a39fc
Aaron Gokani <aarongokani@hotmail.com>
2019-10-10 02:08:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
 # code your #valid_move? method here
-
+def valid_move?(choice)
+  if choice >= 1 && choice <= 9
+    return true
+  else
+    return false
+  end
+end
+  
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken(board, position)
+  if valid_move?
+    if board[position] == " " || board[position] == "" || board[position] == nil
+      return true
+    else
+      return false
+    end
+    
+  end
+end
